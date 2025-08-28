44d80481fd6093f339cf4f9e844f5cef33173aa7
alexgm13 <alexguzmanmiyashiro@gmail.com>
2025-08-27 22:33:26 -0500

    Cross-site scripting 309

--- Code/Dev.1.0/HHMM/HHMM.AppWeb/Filter/frSeguridad.cs
+++ Code/Dev.1.0/HHMM/HHMM.AppWeb/Filter/frSeguridad.cs
@@ -8,6 +8,7 @@ using System.Web.Routing;
 using System.Web.Http;
 using HHMM.Librerias.ReglasNegocio;
 using General.Librerias.CodigoUsuario;
+using System.Net;
 namespace HHMM.AppWeb.Filter
 {
 	public class frSeguridad : ActionFilterAttribute
@@ -28,8 +29,10 @@ namespace HHMM.AppWeb.Filter
 			if (filterContext.HttpContext.Session["Usuario" + id] == null && filterContext.HttpContext.Session["Caduco" + id] == null)
 			{
 				filterContext.HttpContext.Response.Clear();
+				var url = filterContext.HttpContext.Request.Url.Scheme + "://" + filterContext.HttpContext.Request.Url.Authority + filterContext.HttpContext.Request.ApplicationPath;
+				var urlSafe = WebUtility.HtmlEncode(url);
 				//filterContext.HttpContext.Response.Write("<script>window.parent.parent.location.href='http://192.168.1.10/HHMM/HHMM.AppWeb/';</script>");
-				filterContext.HttpContext.Response.Write("reload<script>window.parent.parent.location.href='" + filterContext.HttpContext.Request.Url.Scheme + "://" + filterContext.HttpContext.Request.Url.Authority + filterContext.HttpContext.Request.ApplicationPath + "';</script>");
+				filterContext.HttpContext.Response.Write("reload<script>window.parent.parent.location.href='" + urlSafe + "';</script>");
 			}
 		}
 	}
